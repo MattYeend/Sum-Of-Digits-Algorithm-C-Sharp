651db01df8e2209928aa659ce7b906b0386afe3a
Matt Yeend <mattyeend1@hotmail.co.uk>
2021-03-01 15:58:58 +0000

    Initial Commit

---
+++ Sum Of Digits Algorithm/Sum Of Digits Algorithm/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_Of_Digits_Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, sum = 0, m;
+            Console.Write("Enter a number: ");
+            n = int.Parse(Console.ReadLine());
+            while(n > 0)
+            {
+                m = n % 10;
+                sum = sum + m;
+                n = n / 10;
+            }
+            Console.Write("The sum is: " + sum);
+            Console.ReadKey();
+        }
+    }
+}
